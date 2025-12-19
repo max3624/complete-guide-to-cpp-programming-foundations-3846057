c97dd6a839830c7cdf2da9b4611fee827c60f682
max3624 <130151851+max3624@users.noreply.github.com>
2025-12-19 11:29:33 +0000

    comment add for test

--- src/Ch00/CodeDemo.cpp
+++ src/Ch00/CodeDemo.cpp
@@ -1,5 +1,5 @@
 // Complete Guide to C++ Programming Foundations
-// Exercise 00_03
+// Exercise 00_03 _test
 // Using the Exercise Files in GitHub Codespaces, by Eduardo Corpeño 
 
 #include <iostream>
@@ -11,7 +11,7 @@ int main(){
     std::cin >> num_1;
     std::cout << "Enter number 2: " << std::flush;
     std::cin >> num_2;
-    
+
     result = num_1 + num_2;
     
     std::cout << "The result of the addition is " << result << std::endl;
